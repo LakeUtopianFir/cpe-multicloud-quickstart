650bec274c5941d677d26d1113b09768ad5d3068
Michael Cooke <michael.cooke@genesys.com>
2022-07-28 12:00:04 -0300

    Revert "Update pullsecret"
    This reverts commit 178bd21d5b48891d523b78c42e6964c97acd6715.

--- services/provision-gauth.sh
+++ services/provision-gauth.sh
@@ -10,7 +10,6 @@ export DOMAIN=$VDOMAIN
 export IMAGE_REGISTRY=$VIMAGEREGISTRY
 export ARTIFACT_REPO=$VARTIFACTREPO
 export FULLCOMMAND=$VHELMCOMMAND
-export pullsecret=$PULLSECRET
 
 echo "***********************"
 echo "Logging into GCP"
@@ -32,22 +31,6 @@ else
     echo "Namespace $NS already exists. Will use it."
 fi
 kubectl config set-context --current --namespace=$NS
-
-echo "***********************"
-echo "Crating Pull Secret"
-echo "***********************"
-cat <<EOF | kubectl create -n $NS -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: pullsecret
-  namespace: nexus
-data:
-  .dockerconfigjson: >-
-    $pullsecret
-type: kubernetes.io/dockerconfigjson
-EOF
-
 
 echo "***********************"
 echo "Creating JKS Keystore"
