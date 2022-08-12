15b37c94ba63c48e2680bcfa02b479a971bd7a24
Michael Cooke <77636083+LakeUtopianFir@users.noreply.github.com>
2022-08-11 23:25:42 -0300

    Create gcloud-secrets.sh

---
+++ services/gcloud-secrets.sh
@@ -0,0 +1,29 @@
+echo "***********************"
+echo "Set variables"
+echo "***********************"
+export gkeCluster=$VGKECLUSTER
+export gcpRegion=$VGCPREGION
+export gcpProject=$VGCPPROJECT
+export NS=gauth
+export SERVICE=gauth
+export DOMAIN=$VDOMAIN
+export IMAGE_REGISTRY=$VIMAGEREGISTRY
+export ARTIFACT_REPO=$VARTIFACTREPO
+export FULLCOMMAND=$VHELMCOMMAND
+
+echo "***********************"
+echo "Logging into GCP"
+echo "***********************"
+gcloud init --no-launch-browser
+
+echo "***********************"
+echo "Logging into GKE"
+echo "***********************"
+gcloud container clusters get-credentials $gkeCluster --region $gcpRegion --project $gcpProject
+
+#### Run gcloud commands
+echo 'running gcloud secret'
+secret1=$(gcloud secrets versions access 1 --secret="CCID")
+echo $secret1
+
+echo #### END ##### 
